6f397b604be7c8aef95abaa1cc49f3773b40fe40
Tejsvi-Barad <tejsvi125@gmail.com>
2026-09-21 02:35:36 +0000

    file added

---
+++ variables/display.py
@@ -0,0 +1,6 @@
+name = "Tejsvi"
+age = 19 
+city = "Rajkot"
+
+print(name)
+print(f"My name is {name}, my age is {age} and my city is {city}.")
---
+++ variables/swapping.py
@@ -0,0 +1,17 @@
+a = 10
+b =12
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
+#without variable 
+a=13
+b=12
+a = a+b
+b = a-b
+a = a-b
+
+print(a)
+print(b)
